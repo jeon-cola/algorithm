c6678027b21a0965a294b3fc62f921535d53db7b
jeon-cola <hghg990124@naver.com>
2025-05-31 21:04:17 +0900

    [level 3] Title: 홀짝트리, Time: 1610.34 ms, Memory: 242 MB -BaekjoonHub

--- 프로그래머스/3/388354. 홀짝트리/홀짝트리.js
+++ 프로그래머스/3/388354. 홀짝트리/홀짝트리.js
@@ -1,84 +1,80 @@
 function solution(nodes, edges) {
-    const nodeList = Array(nodes.length);
-    const map = new Map();
-    const count = new Map();
-    const result = [0, 0];
+    const list = Array.from({length:nodes.length}, () => 0)
+    const map = new Map()
+    const count = new Map()
+    const result = [0,0]
 
-    for (let i = 0; i < nodes.length; i++) {
-        nodeList[i] = { weight: nodes[i], childs: [] };
-        map.set(nodes[i], i);
-        count.set(nodes[i], 0);
+    for (let i =0; i<nodes.length; i++) {
+        list[i] = {weight: nodes[i], child:[]}
+        map.set(nodes[i],i)
+        count.set(nodes[i],0)
     }
 
-    for (const [a, b] of edges) {
-        nodeList[map.get(a)].childs.push(b);
-        nodeList[map.get(b)].childs.push(a);
-        count.set(a, (count.get(a) || 0) + 1);
-        count.set(b, (count.get(b) || 0) + 1);
+    for (const [a,b] of edges) {
+        list[map.get(a)].child.push(b)
+        list[map.get(b)].child.push(a)
+        count.set(a,(count.get(a) || 0) +1)
+        count.set(b,(count.get(b)||0)+1)
     }
 
-    function isValid(root, start, visited) {
-        let cnt = count.get(start);
-        if (root !== start) cnt--;
+    function isValid(root, start) {
+        let cnt = count.get(start)
+        if (root !== start ) cnt--
 
         if ((start & 1) === 0) {
-            return (cnt & 1) === 0;
+            return ((cnt & 1) === 0)
         } else {
-            return (cnt & 1) === 1;
+            return ((cnt & 1) === 1)
         }
     }
 
     function solve(root, visited) {
-        visited.add(root);
-        const stk = [root];
+        visited.add(root)
+        const q = [root]
 
-        while (stk.length > 0) {
-            const p = stk.pop();
+        while (q.length > 0) {
+            const current = q.pop()
 
-            for (const next of nodeList[map.get(p)].childs) {
-                if (visited.has(next)) continue;
-                if (!isValid(root, next, visited)) return false;
-                stk.push(next);
-                visited.add(next);
+            for (const next of list[map.get(current)].child) {
+                if (visited.has(next)) continue
+                if (!isValid(root,next)) return false
+                visited.add(next)
+                q.push(next)
             }
         }
-
-        return true;
+        return true
     }
 
     function reverse(root, visited) {
-        visited.add(root);
-        const stk = [root];
+        visited.add(root)
+        const q = [root]
 
-        while (stk.length > 0) {
-            const p = stk.pop();
+        while (q.length > 0) {
+            const current = q.pop()
 
-            for (const next of nodeList[map.get(p)].childs) {
-                if (visited.has(next)) continue;
-                if (isValid(root, next, visited)) return false;
-                stk.push(next);
-                visited.add(next);
+            for (const next of list[map.get(current)].child) {
+                if (visited.has(next)) continue
+                if (isValid(root, next)) return false
+                q.push(next)
+                visited.add(next)
             }
         }
-
-        return true;
+        return true
     }
 
     for (const root of nodes) {
-        let visited = new Set();
-        if (isValid(root, root, visited)) {
-            if (solve(root, visited)) {
-                result[0]++;
+        let visited = new Set()
+        if (isValid(root,root)){
+            if (solve(root,visited)) {
+                result[0]++
             }
         }
-
-        visited = new Set();
-        if (!isValid(root, root, visited)) {
-            if (reverse(root, visited)) {
-                result[1]++;
+        visited = new Set()
+        if (!isValid(root,root)){
+            if (reverse(root,visited)) {
+                result[1]++
             }
         }
     }
-
-    return result;
+    return result
 }
